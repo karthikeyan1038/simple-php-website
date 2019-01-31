e4eeb1d3e810d69c37d4c3cc974660e43cd2c8ea
karthikeyan1038 <45891803+karthikeyan1038@users.noreply.github.com>
2019-01-31 17:25:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 // Comment these lines to hide errors
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 1)
 
-require 'includes/config.php';
+require 'includes/config.php'
 require 'includes
 
-init();
+init()
